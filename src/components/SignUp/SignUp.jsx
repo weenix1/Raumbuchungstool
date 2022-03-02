66dc7458f37b7e2e2afdd4f9b8522742b67b17cf
Weenix Star <60514231+weenix1@users.noreply.github.com>
2022-03-02 12:09:51 +0100

    Update SignUp.jsx

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-import { FormControl } from "react-bootstrap";
+import { Card, FormControl } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { userAccountAtom } from "../../atoms/atoms";
@@ -36,80 +36,92 @@ const SignUp = () => {
     <div className="registration ">
       <Container className="pt-5">
         <Card>
-        <div className="SignUpForm">
-          <h1 className="SignInHeading">SIGN UP</h1>
-          <div noValidate>
-            <Form.Group>
-              <Form.Control
-                size="md"
-                className="SignUpFormControls mt-3"
-                type="text"
-                name="firstName"
-                value={userAccount.firstName}
-                onChange={(e) =>
-                  setUserAccount({ ...userAccount, firstName: e.target.value })
-                }
-                placeholder="Firstname"
-              />
-            </Form.Group>
+          <div className="SignUpForm">
+            <h1 className="SignInHeading">SIGN UP</h1>
+            <div noValidate>
+              <Card.Body>
+                <Form.Group>
+                  <Form.Control
+                    size="md"
+                    className="SignUpFormControls mt-3"
+                    type="text"
+                    name="firstName"
+                    value={userAccount.firstName}
+                    onChange={(e) =>
+                      setUserAccount({
+                        ...userAccount,
+                        firstName: e.target.value,
+                      })
+                    }
+                    placeholder="Firstname"
+                  />
+                </Form.Group>
 
-            <Form.Group>
-              <Form.Control
-                size="md"
-                className=" mt-3"
-                type="text"
-                name="surName"
-                value={userAccount.surName}
-                onChange={(e) =>
-                  setUserAccount({ ...userAccount, surName: e.target.value })
-                }
-                placeholder="Surname"
-              />
-            </Form.Group>
+                <Form.Group>
+                  <Form.Control
+                    size="md"
+                    className=" mt-3"
+                    type="text"
+                    name="surName"
+                    value={userAccount.surName}
+                    onChange={(e) =>
+                      setUserAccount({
+                        ...userAccount,
+                        surName: e.target.value,
+                      })
+                    }
+                    placeholder="Surname"
+                  />
+                </Form.Group>
 
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control
-                type="email"
-                placeholder="Email"
-                value={userAccount.email}
-                onChange={(e) =>
-                  setUserAccount({ ...userAccount, email: e.target.value })
-                }
-                name="email"
-                className=" mt-3"
-                size="md"
-              />
-            </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    value={userAccount.email}
+                    onChange={(e) =>
+                      setUserAccount({ ...userAccount, email: e.target.value })
+                    }
+                    name="email"
+                    className=" mt-3"
+                    size="md"
+                  />
+                </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control
-                className=" mt-3"
-                size="md"
-                type="password"
-                name="password"
-                value={userAccount.password}
-                onChange={(e) =>
-                  setUserAccount({ ...userAccount, password: e.target.value })
-                }
-                placeholder="Password"
-              />
-            </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Control
+                    className=" mt-3"
+                    size="md"
+                    type="password"
+                    name="password"
+                    value={userAccount.password}
+                    onChange={(e) =>
+                      setUserAccount({
+                        ...userAccount,
+                        password: e.target.value,
+                      })
+                    }
+                    placeholder="Password"
+                  />
+                </Form.Group>
 
-            <Button
-              variant="primary"
-              className="SignUpButton mt-3"
-              onClick={registerUser}
-            >
-              Sign Up
-            </Button>
-            <Form.Text>
-              Already a User?{" "}
-              <Link to="/login">
-                <a href="#signin">Sign In</a>
-              </Link>
-            </Form.Text>
+                <Button
+                  variant="primary"
+                  className="SignUpButton mt-3"
+                  onClick={registerUser}
+                >
+                  Sign Up
+                </Button>
+                <Form.Text>
+                  Already a User?{" "}
+                  <Link to="/login">
+                    <a href="#signin">Sign In</a>
+                  </Link>
+                </Form.Text>
+              </Card.Body>
+            </div>
           </div>
-        </div>
+        </Card>
       </Container>
     </div>
   );
